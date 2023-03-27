b06f8d7cfde968d0097977ef4d756b57216c6a78
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-03-27 01:52:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ month = now.month
 day_of_the_week = now.weekday()
 if year == 2020:
     print("The planned pandemic has begun")
+if year == 2023:
+    print("The year is 2023, the fall of the global banking system has begun")
 print(year)
 print(month)
 print(day_of_the_week)
 
 date_of_birth = dt.datetime(year=1980, month=12, day=31)
-print(date_of_birth)
+print(f"You were born on {date_of_birth}, you are old as hell!")
+
